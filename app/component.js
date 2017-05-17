5afea31c647cdd6d803fdebf8bdd99ddd5b9a522
hank <henrik@hankolsen.com>
2017-05-17 22:49:57 +0200

    End of Chapter 3

--- app/component.js
+++ app/component.js
@@ -1,6 +1,6 @@
 export default (text = 'Hej på dig du!') => {
-	const element = document.createElement('div');
-	element.innerHTML = text;
+  const element = document.createElement('div');
+  element.innerHTML = text;
 
-	return element;
+  return element;
 };
--- app/index.js
+++ app/index.js
@@ -1,2 +1,3 @@
 import component from './component';
+
 document.body.appendChild(component());
